f22555e3f75ff945fdd14e5069791d09a0906bbf
Camila Alba <calba@udesa.edu.ar>
2026-06-12 21:24:25 -0300

    corrección de tunel, pantalla de ready, corrección de ghosthouse, y pantalla de modos

--- pacman.py
+++ pacman.py
@@ -15,36 +15,41 @@ class Pacman:
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto) #crea una "caja" que lo rodea
         self.dir_actual = (0, 0)
         self.dir_deseada = (0, 0)    
-    def move(self, lista_paredes, teclas):
+    def move(self, lista_paredes, lista_ghost_house, teclas):
         """
         lista_paredes: En el main, es necesario que haya un for que lea el mapa y guarde las paredes en una lista
         """
     #asignar un significado a cada tecla que es presionada. (wasd)
     #"recuerda" dónde quiere moverse el usuario, para poder ver si puede o no
-        if teclas [pygame.K_a]:
-            self.dir_deseada = (-self.velocidad, 0) #izquierda
-        if teclas [pygame.K_d]:
-            self.dir_deseada = (self.velocidad, 0) #derecha
-        if teclas [pygame.K_w]:
-            self.dir_deseada = (0, -self.velocidad) #arriba
-        if teclas [pygame.K_s]:
-            self.dir_deseada = (0, self.velocidad) #abajo
-        
+        ancho_pantalla = 616  
+        en_tunel = self.x < 0 or self.x > ancho_pantalla - self.ancho
+
+        if not en_tunel:
+            if teclas [pygame.K_a]:
+                self.dir_deseada = (-self.velocidad, 0) #izquierda
+            if teclas [pygame.K_d]:
+                self.dir_deseada = (self.velocidad, 0) #derecha
+            if teclas [pygame.K_w]:
+                self.dir_deseada = (0, -self.velocidad) #arriba
+            if teclas [pygame.K_s]:
+                self.dir_deseada = (0, self.velocidad) #abajo
+    
         dx_deseado, dy_deseado = self.dir_deseada
         dx_actual, dy_actual = self.dir_actual 
         
         pos_x_original = self.x
         pos_y_original = self.y
 
-        if dx_deseado != 0 and dy_actual != 0:
-            self.y = round(self.y / 22) * 22
-        elif dy_deseado != 0 and dx_actual != 0: 
-            self.x = round(self.x / 22) * 22
+        if not en_tunel:
+            if dx_deseado != 0 and dy_actual != 0:
+                self.y = round(self.y / 22) * 22
+            elif dy_deseado != 0 and dx_actual != 0:
+                self.x = round(self.x / 22) * 22
 
         self.rect.x = self.x
         self.rect.y = self.y
 
-        if self.puede_moverse(dx_deseado, dy_deseado, lista_paredes): #si se puede mover a esa casilla, la dirección actual cambia a la deseada. 
+        if not en_tunel and self.puede_moverse(dx_deseado, dy_deseado, lista_paredes, lista_ghost_house): #si se puede mover a esa casilla, la dirección actual cambia a la deseada. 
             self.dir_actual = self.dir_deseada
         else:
             self.x = pos_x_original
@@ -53,18 +58,19 @@ class Pacman:
             self.rect.y = self.y
         
         dx_actual, dy_actual = self.dir_actual 
-        if self.puede_moverse(dx_actual, dy_actual, lista_paredes):
+        if self.puede_moverse(dx_actual, dy_actual, lista_paredes, lista_ghost_house):
             self.x += dx_actual
             self.y += dy_actual 
         else: 
             self.dir_actual = (0, 0)
         #que cruce el tunel y aparezca del otro extremo
         
-        ancho_pantalla = 616 
         if self.x < -22:
             self.x = ancho_pantalla 
+            self.dir_deseada = self.dir_actual
         elif self.x > ancho_pantalla:
             self.x = -22
+            self.dir_deseada = self.dir_actual
 
         self.rect.x = self.x
         self.rect.y = self.y 
@@ -75,7 +81,7 @@ class Pacman:
         """
         return self.rect.colliderect(vecino_rect) #devuelve true si se choca con ese vecino. 
     
-    def puede_moverse(self, dx, dy, lista_paredes):
+    def puede_moverse(self, dx, dy, lista_paredes, lista_ghost_house_rect):
         """
         dx, dy: es el cambio de posición, para saber a qué lado está yendo
         lista_paredes: lista con todas las paredes del mapa
@@ -86,6 +92,9 @@ class Pacman:
         rect_futuro = pygame.Rect(self.x + dx + (margen // 2), self.y + dy + (margen // 2), self.ancho - margen, self.alto - margen)
         for pared in lista_paredes: #revisa todas las paredes del mapa
             if rect_futuro.colliderect(pared.rect):
+                return False 
+        for casa_ghost in lista_ghost_house_rect:
+            if rect_futuro.colliderect(casa_ghost.rect):
                 return False 
         return True
     
